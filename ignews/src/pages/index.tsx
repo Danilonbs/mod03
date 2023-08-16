4c7da54a3e9a985414a1a13ae4a730f85d935fcc
Danilonbs <112011887+Danilonbs@users.noreply.github.com>
2023-08-16 11:41:39 -0300

    add header and styles

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,15 @@
-import '../styles/home.module.css'
+
+import Head from 'next/head'
 
 export default function Home() {
     return (
-        <h1>Hello Word</h1>
+        <>
+        <Head>
+            <title>Início | ig.news</title>
+        </Head>
+        <h1 >
+            Hello Word
+        </h1>
+        </>
     )
-}
+} 
